2970103456ba799381827c3ed2f02905736f524e
NitheshHS <66508499+NitheshHS@users.noreply.github.com>
2021-06-02 17:07:33 +0530

    changes are made

--- SDET_13_GIT/src/test/java/com/sdet13git/demo/Branching.java
+++ SDET_13_GIT/src/test/java/com/sdet13git/demo/Branching.java
@@ -1,7 +1,15 @@
 package com.sdet13git.demo;
 
+/**
+ * 
+ * @author Nitheesha
+ *
+ */
 public class Branching {
-
+/**
+ * This is main method added
+ * @param args
+ */
 	public static void main(String[] args) {
 		System.out.println("push the code to new branch");
 	}
