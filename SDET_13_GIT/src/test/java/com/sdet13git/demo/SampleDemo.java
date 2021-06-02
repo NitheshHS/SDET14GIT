90a0a29eafedc207b176e79de8d5e62210bae9f8
NitheshHS <66508499+NitheshHS@users.noreply.github.com>
2021-06-02 16:44:14 +0530

    User2

--- SDET_13_GIT/src/test/java/com/sdet13git/demo/SampleDemo.java
+++ SDET_13_GIT/src/test/java/com/sdet13git/demo/SampleDemo.java
@@ -4,6 +4,9 @@ public class SampleDemo {
 	
 	public static void main(String[] args) {
 		System.out.println("I am the user 2 for the project");
+		
+		
+		System.out.println("user2 trying to push a code");
 	}
 
 }
